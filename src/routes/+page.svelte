c2a31f1d2e3e163b920368e76ef54d97cc0988ca
MaksimZadar <maxzadarnovskiyy@gmail.com>
2024-09-07 13:43:36 +0930

    install carbon ui theme

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
+<script lang="ts">
+    import "carbon-components-svelte/css/all.css";
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
