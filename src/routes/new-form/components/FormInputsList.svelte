0288c55fc7d7122eb57f2faabd078201c9db60bb
MaksimZadar <maxzadarnovskiyy@gmail.com>
2024-09-08 21:36:43 +0930

    use the new inputs list

--- src/routes/new-form/components/FormInputsList.svelte
+++ src/routes/new-form/components/FormInputsList.svelte
@@ -1,9 +1,9 @@
 <script>
-  import { ComponentSelectionList } from "$lib/form-components/FormComponentType";
+  import { InputSelectionList } from "$lib/form-components/FormComponentType";
 </script>
 
 <div class="w-full h-full overflow-hidden overflow-y-auto">
-  {#each ComponentSelectionList as component}
-    <svelte:component this={component} />
+  {#each InputSelectionList as input}
+    <svelte:component this={input} />
   {/each}
 </div>
