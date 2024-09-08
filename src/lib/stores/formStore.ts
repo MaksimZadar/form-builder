859d4c4f1c75929001f68bd093f98b9d6ad94192
MaksimZadar <maxzadarnovskiyy@gmail.com>
2024-09-08 10:40:47 +0930

    create form store

---
+++ src/lib/stores/formStore.ts
@@ -0,0 +1,60 @@
+import {
+  type FormComponent,
+  type FormData,
+} from "$lib/form-components/FormComponentType";
+import { writable } from "svelte/store";
+
+function createFormStore() {
+  const { subscribe, set, update } = writable<FormData>({
+    formName: "New Form",
+    formComponents: [
+      // {
+      //   type: FormComponentTypesEnum.SIMPLE_INPUT,
+      //   component: SimpleInput,
+      //   props: {
+      //     label: "Name",
+      //     placeholder: "Enter your name",
+      //   },
+      // },
+    ],
+  });
+
+  return {
+    subscribe,
+    set,
+    update,
+    updateFormName: (newFormName: string) => {
+      update((form) => ({ ...form, formName: newFormName }));
+    },
+    addFormComponent: (formComponent: FormComponent) => {
+      update((form) => ({
+        ...form,
+        formComponents: [...form.formComponents, formComponent],
+      }));
+    },
+    removeFormComponent: (index: number) => {
+      update((form) => ({
+        ...form,
+        formComponents: form.formComponents.filter((_, i) => i !== index),
+      }));
+    },
+    moveFormComponent: (dragIndex: number, index: number) => {
+      update((form) => {
+        const dragItem = form.formComponents[dragIndex];
+        form.formComponents = form.formComponents.filter(
+          (_, i) => i !== dragIndex
+        );
+        return {
+          ...form,
+          formComponents: [
+            ...form.formComponents.slice(0, index),
+            dragItem,
+            ...form.formComponents.slice(index),
+          ],
+        };
+      });
+    },
+  };
+}
+
+export const formStore = createFormStore();
