62f242c6be62ae5cb683394d7244d7de6c8d1ae1
MaksimZadar <maxzadarnovskiyy@gmail.com>
2024-09-08 10:38:51 +0930

    apply theme site wide

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import { Theme } from "carbon-components-svelte";
+  import "carbon-components-svelte/css/all.css";
+  import "../app.css";
+  import { themePersistKey, themeStore } from "../lib/stores/themeStore";
+</script>
+
+<Theme bind:theme={$themeStore} persist persistKey={themePersistKey} />
+
+<slot />
